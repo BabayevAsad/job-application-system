19175a7a0023b0bdc1938dc99e0b574e8a90b617
BabayevAsad <esed.babayev2002@gmail.com>
2025-09-07 01:17:39 +0200

    add path to SpringProjectApplicationTests

--- src/test/java/com/AsadBabayev/SpringProjectApplicationTests.java
+++ src/test/java/com/AsadBabayev/SpringProjectApplicationTests.java
@@ -1,9 +1,10 @@
 package com.AsadBabayev;
 
+import com.AsadBabayev.starter.SpringProjectApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringProjectApplication.class)
 class SpringProjectApplicationTests {
 
 	@Test
